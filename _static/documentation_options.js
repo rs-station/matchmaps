07f8e0c62572a152c1de8dae1b4a20894dcdd1d8
GitHub Action <action@github.com>
2023-07-11 01:19:30 +0000

    Upload documentation from latest commit

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g6e8fabc',
+    VERSION: '0.1.dev1+g98e1df8',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
